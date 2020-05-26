485b3ee1d47af028c9a9d747eb2961b66bd9120c
han <32795466+hb1005642457@users.noreply.github.com>
2020-05-26 22:37:28 +0800

    first learn
    feat(0525): vue jichu

---
+++ kaikeba/20200525/my-test/hello.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+        watch: {
+            data(newValue, oldValue) {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
